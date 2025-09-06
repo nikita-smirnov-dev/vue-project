1419e8451a2c8a1a378fae7f2d296222d823d9e8
Nikita Smirnov <nikita_smirnov91@mail.ru>
2025-09-06 17:52:22 +0300

    initial commit

---
+++ project/src/main.ts
@@ -0,0 +1,17 @@
+import { createApp } from 'vue';
+import { createPinia } from 'pinia';
+
+import App from './App.vue';
+import router from './router';
+
+import './assets/styles/normalize.css';
+import './assets/styles/variables.css';
+import './assets/styles/fonts.css';
+import './assets/styles/main.css';
+
+const app = createApp(App);
+
+app.use(createPinia());
+app.use(router);
+
+app.mount('#app');
